ee87a22aef6c80e31c1102e6d63b63e4a1193750
Grid112 <sumitware1999@gmail.com>
2021-09-16 23:35:33 +0530

    To Add Part Time Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,24 +4,32 @@ import java.util.Random;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int IS_FULL_TIME = 1;
+        int IS_FULL_TIME1 = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
-        // Taking Random values 0 or 1 to Check Attendance of Employee
+        // Taking Random values 0 or 1. 0==Absent and 1==Present
         Random random = new Random();
         int empCheck = random.nextInt(2);
-        // Checking the condition. 0==Absent And 1==Present
+        int empCheck1 = random.nextInt(3);
+        // Checking the condition.
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present");
-            empHours = 8;
+            if (empCheck1 == IS_FULL_TIME1) {
+                empHours = 8;
+            }
+            else if (empCheck1 == IS_PART_TIME) {
+                empHours = 4;
+            } else {
+                empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            System.out.println("The daily wages of an employee is : " + empWage);
         }
         else {
             System.out.println("Employee is Absent");
-            empHours = 0;
         }
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        System.out.println("The daily wages of an employee is : " + empWage);
     }
 }
